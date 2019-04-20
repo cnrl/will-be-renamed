033ba448e145180175dd453dde16107bcc5147d8
atenagm1375 <ashenatena@gmail.com>
2019-04-20 14:11:37 +0430

    Add Connection class

---
+++ Connection.py
@@ -0,0 +1,11 @@
+import inspect
+from copy import deepcopy
+
+class Connection:
+    _instance_count = 0
+    def __init__(self, pre, post, connection_type, synapse=None, name=None):
+        self.pre = pre
+        self.post = post
+        self.connection_type = connection_type
+        self.synapse = synapse() if inspect.isclass(synapse) else deepcopy(synapse)
+        self.name = name or "Connection_{}".format(self._instance_count)
