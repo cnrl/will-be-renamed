6a089a90f553511c1118abfb62d2b8174d487166
atenagm1375 <ashenatena@gmail.com>
2019-04-24 15:43:13 +0430

    add documentation for Synapse class

--- Synapse.py
+++ Synapse.py
@@ -2,11 +2,22 @@
 class Synapse(object):
 
     """
-        TODO: Documentation
+        Class to define a synapse
     """
     _instance_count = 0
 
     def __init__(self, parameters='', equations='', functions=None, pre=None, post=None, psp=None, name=None):
+        """
+            Parameters:
+
+            > parameters: Parameters of the synapse and their initial values.
+            > equations: Equations of the synapse, defining the temporal evolution of variables.
+            > functions: Definition of additional functions used in `equations`.
+            > pre: Variable updates when a pre-synaptic spike is received.
+            > post: Variable updates when a post-synaptic spike is emitted.
+            > psp: Influence of a synapse on post-synaptic neuron.
+            > name: Name of the neuron.
+        """
         self.parameters = parameters
         self.equations = equations
         self.pre = pre
