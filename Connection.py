59b810731f98a3f15cb892eef41f2b91c18e1364
atenagm1375 <ashenatena@gmail.com>
2019-04-24 15:10:56 +0430

    add representation function to Connection class

--- Connection.py
+++ Connection.py
@@ -2,10 +2,30 @@ import inspect
 from copy import deepcopy
 
 class Connection:
+    """
+        TODO: Documentation
+    """
     _instance_count = 0
+
     def __init__(self, pre, post, connection_type, synapse=None, name=None):
         self.pre = pre
         self.post = post
         self.connection_type = connection_type
         self.synapse = synapse() if inspect.isclass(synapse) else deepcopy(synapse)
         self.name = name or "Connection_{}".format(self._instance_count)
+
+        self._instance_count += 1
+
+    def __repr__(self):
+        text = self.name + """
+
+        Pre synaptic population:
+        """ + str(self.pre) + """
+        Post synaptic population:
+        """ + str(self.post) + """
+        Connection type:
+        """ + str(self.type) + """
+        Synapse:
+        """ + str(self.synapse)
+
+        return text
