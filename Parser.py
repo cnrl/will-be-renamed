cc74a5cdec765cbc0cda4d099870f24323372fad
atenagm1375 <ashenatena@gmail.com>
2019-05-04 19:21:04 +0430

    fix string manipulation to strip spaces around = and :, resolve error due to parameter parsing

--- Parser.py
+++ Parser.py
@@ -1,4 +1,4 @@
-from re import split
+from re import split, compile
 from CustomExceptions import Parameter_name_Exception
 
 
@@ -6,16 +6,19 @@ def parameters_string_manipulation(parameters):
     lines = split("[\n;]", parameters)
     lines = (lambda x: [i.strip() for i in x])(lines)
     # TODO Handle syntax error(apply a regular expression)
-    params = (lambda x: [tuple(split("[=:]", i)) for i in x])(lines)
+    params = (lambda x: [tuple((lambda y: [j.strip() for j in y])(split("[=:]", i))) for i in x])(lines)
     return params
 
 
 def parse_parameters(parameters):
     for param in parameters:
-        pattern = re.compile("[a-zA-Z_]+")
+        print(param)
+        pattern = compile("[a-zA-Z_]+")
         if not pattern.match(param[0]):
-            raise(Parameter_name_Exception("Invalid syntax for name of a parameter"))
+            raise(Parameter_name_Exception("Invalid syntax for name of a parameter: " + param[0]))
+        # TODO: add rest of paarameter parsing and resolve bug for name(khubi? is a valid name :/)
 
-string = """salam=5; khubi?=tof:baqal
+string = """salam = 5 ; khubi?=tof:baqal
             hashem=0"""
-parameters_string_manipulation(string)
+p = parameters_string_manipulation(string)
+parse_parameters(p)
