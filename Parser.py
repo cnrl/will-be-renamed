a4ad9c0e01b5b93c6fd50ffbbfb2fb4357aafb9d
atenagm1375 <ashenatena@gmail.com>
2019-05-10 15:54:55 +0430

    change file so as to use GrammarDefaults based on pyparsing. add parse parameter function

--- Parser.py
+++ Parser.py
@@ -1,24 +1,10 @@
-from re import split, compile
-from CustomExceptions import Parameter_name_Exception
-
-
-def parameters_string_manipulation(parameters):
-    lines = split("[\n;]", parameters)
-    lines = (lambda x: [i.strip() for i in x])(lines)
-    # TODO Handle syntax error(apply a regular expression)
-    params = (lambda x: [tuple((lambda y: [j.strip() for j in y])(split("[=:]", i))) for i in x])(lines)
-    return params
+from GrammarDefaults import parameters_syntax
 
 
 def parse_parameters(parameters):
-    for param in parameters:
-        print(param)
-        pattern = compile("[a-zA-Z_]+")
-        if not pattern.match(param[0]):
-            raise(Parameter_name_Exception("Invalid syntax for name of a parameter: " + param[0]))
-        # TODO: add rest of paarameter parsing and resolve bug for name(khubi? is a valid name :/)
+    return parameters_syntax.parseString(parameters)
 
-string = """salam = 5 ; khubi?=tof:baqal
-            hashem=0"""
-p = parameters_string_manipulation(string)
-parse_parameters(p)
+
+# string = """salam = 5:self; khubi=6:self;
+#             hashem=0;"""
+# print(parse_parameters(string))
