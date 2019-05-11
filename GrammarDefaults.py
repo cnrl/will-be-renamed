8f24049843e60d9ea135dff2bd0e7548cd093ca9
atenagm1375 <ashenatena@gmail.com>
2019-05-11 11:06:14 +0430

    add signed int for values

--- GrammarDefaults.py
+++ GrammarDefaults.py
@@ -2,15 +2,18 @@ import pyparsing as pp
 from General import named_constants
 
 
+# parameter grammar
 _first = pp.Word(pp.alphas + "_", exact=1)
 _rest = pp.Word(pp.alphanums + "_")
 param_name = pp.Combine(_first + pp.Optional(_rest)).setResultsName("param name")
 
 _int = pp.Word(pp.nums)
-_long_int = pp.Combine(pp.Word(pp.nums) + pp.Optional(pp.Literal("L")))
-_double = pp.Or([pp.Combine(pp.Optional(_int) + "." + _int), \
-                    pp.Combine(_int + "." + pp.Optional(_int))])
-param_value = pp.Or([_int, _long_int, _double] + \
+_sign = pp.Optional(pp.Or(pp.Literal("-") + pp.Literal("+")))
+_signed_int = _sign + _int
+_long_int = pp.Combine(_sign + pp.Word(pp.nums) + pp.Optional(pp.Literal("L")))
+_double = pp.Or([pp.Combine(pp.Optional(_signed_int) + "." + _int), \
+                    pp.Combine(_signed_int + "." + pp.Optional(_int))])
+param_value = pp.Or([_signed_int, _long_int, _double] + \
                 (lambda x: [pp.Literal(i) for i in x])(named_constants) \
                 ).setResultsName("param value")
 
@@ -19,3 +22,7 @@ param_scope = pp.Or((lambda x: [pp.Literal(i) for i in x])(_scope_literals)).set
 
 param_syntax = param_name + "=" + param_value + pp.Optional(":" + param_scope)
 parameters_syntax = pp.OneOrMore(param_syntax + ";")
+
+
+# equation grammar
+
