4ebebc023cc1b82a08cee956637eade02d9354cc
Alireza Mohammadi <daban.am56@gmail.com>
2019-05-16 20:55:23 +0430

    added _create_dirs function

--- cnrl/code_generators/api.py
+++ cnrl/code_generators/api.py
@@ -1,13 +1,19 @@
+import os
 from cnrl.code_generators.models import Population, Projection
-from cnrl.exceptions import IllegalArgument
+from cnrl.exceptions import IllegalArgument, IllegalState
 
 
 # generates cpp code, compiles it and returns dynamically loaded module
-def generate(pops, projs):
+def generate(name, pops, projs):
     _check_args(pops, projs)
 
+    base_path = _create_dirs(name)
 
-def _check_args(pops, projs):
+
+def _check_args(name, pops, projs):
+    if type(name) != str:
+        raise IllegalArgument("name must be a string")
+
     if type(pops) != list:
         raise IllegalArgument("pops must be a list")
     for pop in pops:
@@ -19,3 +25,19 @@ def _check_args(pops, projs):
     for proj in projs:
         if not isinstance(proj, Projection):
             raise IllegalArgument("projs must be a list of Projections")
+
+
+def _create_dirs(name):
+    cwd = os.getcwd()
+
+    dir_path = os.path.join(cwd, 'build')
+
+    if os.path.exists(dir_path):
+        raise IllegalState('cannot create build folder, a file or directory with this name exists')
+
+    os.mkdir(dir_path)
+
+    base_path = os.path.join(dir_path, name)
+    os.mkdir(base_path)
+
+    return base_path
--- cnrl/exceptions.py
+++ cnrl/exceptions.py
@@ -1,3 +1,8 @@
 class IllegalArgument(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class IllegalState(Exception):
+    def __init__(self, message):
+        self.message = message
