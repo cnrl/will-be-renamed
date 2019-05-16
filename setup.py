7d0b4aa131f72395f02863478f5d8ef3c14262b0
Alireza Mohammadi <daban.am56@gmail.com>
2019-05-16 21:06:53 +0430

    added Jinja2 to project dependencies

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     author='CNRL Team',
     packages=['cnrl'],
     license='MIT',
-    install_requires=['numpy', 'scikit-image', 'opencv-python', 'pyparsing']
+    install_requires=['numpy', 'scikit-image', 'opencv-python', 'pyparsing', 'Jinja2']
 )
