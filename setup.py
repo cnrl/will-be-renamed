8f473eedd130064361fb2cc57164df0b4d8a29c2
Alireza Mohammadi <daban.am56@gmail.com>
2019-05-16 21:05:04 +0430

    extracted package_name to globals.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
+from cnrl.globals import package_name
 
 # TODO: change package name (name and packages)
 # TODO: change url when package name changed
 # TODO: consider versions in dependencies
 setup(
-    name='cnrl',
+    name=package_name,
     version='0.1.0dev',
     description='A Simulator for Spiking Neural Networks',
     url='https://github.com/cnrl/will-be-renamed',
