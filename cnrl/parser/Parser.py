56e39fcdfb185458fe3f959e7f403e305d9dfd61
atenagm1375 <ashenatena@gmail.com>
2019-05-17 17:58:13 +0430

    sympify parameters and equations

--- cnrl/parser/Parser.py
+++ cnrl/parser/Parser.py
@@ -1,7 +1,20 @@
-def parse_equations(equations):
-    pass
+from sympy import sympify, symbols, Eq
+
+from cnrl.parser.Lexer import parameters_lexer, equations_lexer, variable_lexer
 
 
-# string = """salam = 5:self; khubi=6:self;
-#             hashem=0;"""
-# print(parse_parameters(string))
+def parse_parameters(parameters):
+    return symbols(" ".join([param["name"] for param in parameters_lexer(parameters)]))
+
+
+def parse_equations(equations, parameters):
+    equations = equations_lexer(equations)
+    variables = variable_lexer(equations)
+    parameters = parse_parameters(parameters)
+    variables = symbols(" ".join([var["name"] for var in variables]))
+    eqs = []
+    for eq in equations:
+        lhs = sympify(eq["lhs"], evaluate=False)
+        rhs = sympify(eq["rhs"], evaluate=False)
+        eqs.append(Eq(lhs, rhs))
+    return eqs, parameters, variables
