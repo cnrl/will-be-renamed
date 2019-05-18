f99f87170afdee4675346c2a06a6eb41ef34cd9d
atenagm1375 <ashenatena@gmail.com>
2019-05-18 16:34:06 +0430

    add parser for mathematical expression and conditional equation

--- cnrl/parser/Parser.py
+++ cnrl/parser/Parser.py
@@ -1,20 +1,26 @@
 from sympy import sympify, symbols, Eq
 
-from cnrl.parser.Lexer import parameters_lexer, equations_lexer, variable_lexer
+from cnrl.parser.Lexer import parameters_lexer, equations_lexer, variable_lexer, conditional_equations_lexer
 
 
 def parse_parameters(parameters):
-    return symbols(" ".join([param["name"] for param in parameters_lexer(parameters)]))
+    return parameters_lexer(parameters)
 
 
-def parse_equations(equations, parameters):
+def parse_equations(equations):
     equations = equations_lexer(equations)
     variables = variable_lexer(equations)
-    parameters = parse_parameters(parameters)
-    variables = symbols(" ".join([var["name"] for var in variables]))
     eqs = []
     for eq in equations:
         lhs = sympify(eq["lhs"], evaluate=False)
         rhs = sympify(eq["rhs"], evaluate=False)
         eqs.append(Eq(lhs, rhs))
-    return eqs, parameters, variables
+    return eqs, variables
+
+
+def parse_conditions(conditions):
+    return conditional_equations_lexer(conditions)
+
+
+def parse_mathematical_expr(expr):
+    return sympify(expr, evaluate=False)
