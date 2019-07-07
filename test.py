31cfa3e7269bac5064daf921c655e22776bbd9c6
atenagm <ashenatena@gmail.com>
2019-07-07 13:50:16 +0430

    add a sample test

---
+++ test.py
@@ -0,0 +1,35 @@
+from cerebro.models import Neuron, Synapse, Network, Population, Connection
+
+neuron = Neuron(
+    variables="""
+        v = 0 : local
+    """,
+    equations="""
+        v = 5
+    """,
+    spike="(v > 5)",
+    reset="""
+        v = 3
+        v = 10
+    """
+)
+
+synapse = Synapse(
+    variables="""
+        p = 0 : shared
+        tau = 12 : constant
+        x = 3
+    """,
+    equations="""
+        x = p + tau - _pre_v + _post_v - n
+    """
+)
+
+pop = Population(neuron=neuron, size=10)
+pop2 = Population(neuron=neuron, size=10)
+
+conn = Connection(pre=pop, post=pop2, synapse=synapse)
+
+net = Network(populations=[pop, pop2], connections=[conn], variables="n = 2")
+
+net.compile()
