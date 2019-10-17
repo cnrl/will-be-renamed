24ecc56f2b7603e62aa0976c93ecb530d07d76cb
atenagm <ashenatena@gmail.com>
2019-10-17 12:12:31 +0330

    add docstring to exceptions.py

--- cerebro/exceptions.py
+++ cerebro/exceptions.py
@@ -1,3 +1,39 @@
+"""Some more informative exceptions
+
+Classes
+----------
+IllegalArgumentException(Exception)
+    Inappropriate argument.
+    Methods defined here:
+        __init__(self, message)
+            initializes self.message with the given message string.
+
+IllegalStateException(Exception)
+    Inappropriate code generation state.
+    Methods defined here:
+        __init__(self, message)
+            initializes self.message with the given message string.
+
+ParseException(Exception)
+    Grammatically-ill string in context variables or equations.
+    Methods defined here:
+        __init__(self, message)
+            initializes self.message with the given message string.
+
+SemanticException(Exception)
+    Semantically-ill string in context variables or equations.
+    Methods defined here:
+        __init__(self, message)
+            initializes self.message with the given message string.
+
+InternalException(Exception)
+    Developmental issues in core code generation.
+    Methods defined here:
+        __init__(self, message)
+            initializes self.message with the given message string.
+"""
+
+
 class IllegalArgumentException(Exception):
     def __init__(self, message):
         self.message = message
