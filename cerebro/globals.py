354bfab90da15f86ebb99f6e8681754b6baf8d8a
atenagm <ashenatena@gmail.com>
2019-10-17 11:50:39 +0330

    add docstring to global.py

--- cerebro/globals.py
+++ cerebro/globals.py
@@ -1,3 +1,27 @@
+"""Objects with a wide range of use in the package
+
+Objects
+----------
+FORBIDDEN_VARIABLE_NAMES : dict of VariableContext: set of str
+    Set of meaningful variables in each context. These variables can not be redefined as variables in a context.
+INTERNAL_VARIABLES : set of str
+    Set of variables that can be explicitly used in an equation in a context.
+RESERVED_WORDS : set of str
+    Set of words that are reserved in the project and cannot be used as name of variables in a context.
+PACKAGE_NAME : str
+    Name of the package.
+VARIABLE_NAME_PATTERN : str
+    Regex template for context variable matches.
+NUMERAL_PATTERN : str
+    Regex template for numerals used in a context equation or variable.
+WORD_PATTERN : str
+    Regex template for words(i.e. non-numerals).
+ACCEPTABLE_CONSTRAINTS : dict of str: dict of str: str/set of str
+    Defines valid values for constraints used in context equations.
+ACCEPTABLE_PROPRIETOR : set of str
+    Defines valid proprietor words in context equations.
+"""
+
 from .enums import VariableContext
 
 FORBIDDEN_VARIABLE_NAMES = {
