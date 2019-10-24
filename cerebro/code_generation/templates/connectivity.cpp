3a5ed36bb9e8916ed399f76bd1093bfb04459e8b
atenagm <ashenatena@gmail.com>
2019-10-24 10:51:46 +0330

    add connect_with_probability and connect_by_func functions to connectivity

--- cerebro/code_generation/templates/connectivity.cpp
+++ cerebro/code_generation/templates/connectivity.cpp
@@ -2,13 +2,27 @@
 
 #include "connectivity.h"
 
+bool all_to_all_func(int pre_rank_idx, int post_rank_idx){
+    return true;
+}
+
 std::vector< std::vector<int> > connect_all_to_all(int pre_size, int post_size){
+    return connect_by_func(all_to_all_func, pre_size, post_size);
+}
+
+bool connect_with_probability(int pre_rank_idx, int post_rank_idx, float prob){
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    return dis(random_generator) < prob;
+}
+
+std::vector< std::vector<int> > connect_by_func(bool (*func)(int, int), int pre_size, int post_size){
     std::vector< std::vector<int> > connectivity;
 
     for (int post_rank_idx = 0;post_rank_idx < post_size; post_rank_idx++) {
         connectivity.push_back(std::vector<int>());
         for (int pre_rank_idx = 0;pre_rank_idx < pre_size; pre_rank_idx++)
-            connectivity.back().push_back(pre_rank_idx);
+            if (func(pre_rank_idx, post_rank_idx))
+                connectivity.back().push_back(pre_rank_idx);
         }
     return connectivity;
 }
--- cerebro/code_generation/templates/connectivity.h
+++ cerebro/code_generation/templates/connectivity.h
@@ -1,4 +1,9 @@
 #include <vector>
+#include <random>
+#include "core.h"
 
-
+extern std::default_random_engine random_generator;
 std::vector< std::vector<int> > connect_all_to_all(int, int);
+bool all_to_all_func(int , int );
+bool connect_with_probability(int , int, float);
+std::vector< std::vector<int> > connect_by_func(bool (*)(int, int), int , int );
